f9b0c8f39e44bcd58e5d91c988bf45893d85420b
dunovank <dunovank@gmail.com>
2015-07-27 23:29:59 -0400

    minor fixes

--- fit.py
+++ fit.py
@@ -54,6 +54,10 @@ class Simulator(object):
             self.kind=kind
             self.method=method
             self.pc_map=pc_map
+            self.pnames=['a', 'tr', 'v', 'ssv', 'z']
+            if self.kind=='proactive':
+                  ssv_val = self.pnames.remove('ssv')
+
             self.pvectors=['a', 'tr', 'v']
             self.pvc=deepcopy(self.pvectors)
             self.flat=False
@@ -67,7 +71,11 @@ class Simulator(object):
             if self.style=='IP':
                   ssv=(abs(ssv[1]), abs(ssv[0]))
 
-            return {'a': a, 'tr': tr, 'v': v, 'ssv': ssv, 'z': z}
+            bounds = {'a': a, 'tr': tr, 'v': v, 'ssv': ssv, 'z': z}
+            if self.kind=='proactive':
+                  ssv = bounds.pop('ssv')
+
+            return bounds
 
 
       def optimize_theta(self, y, inits, flat=False):
@@ -116,40 +124,31 @@ class Simulator(object):
             """
 
             self.y = y.flatten()
-            self.pvc=deepcopy(self.pvectors)
             self.flat=flat
+            self.pvc = deepcopy(self.pvectors)
+            pnames = deepcopy(self.pnames)
             lim = self.set_bounds()
-
-            if self.wts is None:
-                  self.wts=np.ones_like(self.y)
 
             if self.flat:
                   wts = self.flat_wts
             else:
                   wts = self.wts
 
-            if inits!=None:
-                  ip = deepcopy(inits)
-            else:
-                  ip = deepcopy(self.inits)
-
-            if 'pGo' in ip.keys(): del ip['pGo']
-            if 'ssd' in ip.keys(): del ip['ssd']
-
-            if self.style=='IP':
+            ip = deepcopy(inits)
+            if self.kind=='reactive' and self.style=='IP':
                   ip['ssv']=abs(ip['ssv'])
-            elif self.style=='RADD':
+            elif self.kind=='reactive' and self.style=='RADD':
                   ip['ssv']=-abs(ip['ssv'])
 
-            theta=Parameters();
+            theta=Parameters()
             for pkey, pc_list in self.pc_map.items():
                   if self.flat: break
                   self.pvc.remove(pkey)
-                  bv = ip.pop(pkey)
-                  mn = lim[pkey][0]; mx = lim[pkey][1]
-                  d0 = [theta.add(pc, value=bv, vary=True, min=mn, max=mx) for pc in pc_list]
+                  pnames.remove(pkey)
+                  mn = lim[pkey][0]; mx=lim[pkey][1]
+                  d0 = [theta.add(pc, value=ip[pkey], vary=1, min=mn, max=mx) for pc in pc_list]
 
-            p0 = [theta.add(k, value=v, vary=self.flat, min=lim[k][0], max=lim[k][1]) for k, v in ip.items()]
+            p0 = [theta.add(k, value=ip[k], vary=self.flat, min=lim[k][0], max=lim[k][1]) for k in pnames]
             opt_kws = {'disp':self.disp, 'xtol':self.xtol, 'ftol':self.ftol, 'maxfev':self.maxfev}
 
             optmod = minimize(self.cost_fx, theta, method=self.method, kws={'wts':wts}, options=opt_kws)
@@ -242,27 +241,25 @@ class Simulator(object):
                   else:
                         p[pkey] = np.array([p[pc] for pc in pkc])
             if as_dict:
-                  allp = deepcopy(self.pvectors)
-                  allp.extend(['ssv', 'z'])
-                  return {k: p[k][0] if k in self.pvc else p[k] for k in allp}
+                  return {k: p[k][0] if k in self.pvc else p[k] for k in self.pnames}
 
-            out = [p['a'], p['tr'], p['v'], p['ssv'], p['z']]
+            out = [p[k] for k in self.pnames]
 
             if sim_info:
                   Pg = 0.5*(1 + p['v']*self.dx/self.si)
-                  Ps = 0.5*(1 + p['ssv']*self.dx/self.si)
                   Tg = np.ceil((self.tb-p['tr'])/self.dt).astype(int)
-                  Ts = np.ceil((self.tb-self.ssd)/self.dt).astype(int)
-                  out.extend([Pg, Ps, Tg, Ts])
+                  out.extend([Pg, Tg])
+                  if self.kind=='reactive':
+                        Ps = 0.5*(1 + p['ssv']*self.dx/self.si)
+                        Ts = np.ceil((self.tb-self.ssd)/self.dt).astype(int)
+                        out.extend([Ps, Ts])
 
             return out
 
 
-      def core_radd(self, p=None):
+      def core_radd(self, p):
 
-            if p is None:
-                  p=self.inits
-            a, tr, v, ssv, z, Pg, Ps, Tg, Ts = self.vectorize_params(p)
+            a, tr, v, ssv, z, Pg, Tg, Ps, Ts = self.vectorize_params(p)
             # a/tr/v Bias: ALL CONDITIONS, ALL SSD
             DVg = z + np.cumsum(np.where((rs((self.ncond, self.ntot, Tg.max())).T < Pg), self.dx, -self.dx).T, axis=2)
             init_ss = np.array([[DVc[:self.nss, ix] for ix in np.where(Ts<Tg[i], Tg[i]-Ts, 0)] for i, DVc in enumerate(DVg)])
@@ -271,23 +268,18 @@ class Simulator(object):
             return DVg, DVs
 
 
-      def pro_radd(self, p=None):
+      def pro_radd(self, p):
 
-            if p is None:
-                  p=self.inits
-            a, tr, v, ssv, z, Pg, Ps, Tg, Ts = self.vectorize_params(p)
+            a, tr, v, z, Pg, Tg = self.vectorize_params(p)
             ntrials = int(self.ntot/self.ncond)
             # a/tr/v Bias: ALL CONDITIONS
             DVg = z + np.cumsum(np.where((rs((self.ncond, ntrials, Tg.max())).T < Pg), self.dx, -self.dx).T, axis=2)
-
             return DVg
 
 
-      def ip_radd(self, p=None):
+      def ip_radd(self, p):
 
-            if p is None:
-                  p=self.inits
-            a, tr, v, ssv, z, Pg, Ps, Tg, Ts = self.vectorize_params(p)
+            a, tr, v, ssv, z, Pg, Tg, Ps, Ts = self.vectorize_params(p)
             # a/tr/v Bias: ALL CONDITIONS, ALL SSD
             DVg = z + np.cumsum(np.where((rs((self.ncond, self.ntot, Tg.max())).T < Pg), self.dx, -self.dx).T, axis=2)
             init_ss = np.ones((self.ncond, self.nssd, self.nss))*z
@@ -296,10 +288,8 @@ class Simulator(object):
             return DVg, DVs
 
 
-      def analyze_reactive(self, DVg, DVs, p=None):
+      def analyze_reactive(self, DVg, DVs, p):
 
-            if p is None:
-                  p=self.inits
             a, tr, v, ssv, z = self.vectorize_params(p, sim_info=False)
             dt=self.dt; nss=self.nss; ncond=self.ncond; ssd=self.ssd; tb=self.tb; prob=self.prob
 
@@ -322,22 +312,23 @@ class Simulator(object):
             return np.hstack([np.hstack([i[ii] for i in [gac, sacc, gq, eq]]) for ii in range(ncond)])
 
 
-      def analyze_proactive(self, DVg, p=None):
+      def analyze_proactive(self, DVg, p):
 
-            if p is None:
-                  p=self.inits
-            a, tr, v, ssv, z = self.vectorize_params(p, sim_info=False)
-            dt=self.dt; nss=self.nss; ncond=self.ncond; ssd=self.ssd; tb=self.tb; prob=self.prob
+            a, tr, v, z = self.vectorize_params(p, sim_info=False)
+            dt=self.dt; ncond=self.ncond; tb=self.tb; prob=self.prob
 
-            grt = (tr+(np.where((DVg.max(axis=2).T>=a).T, np.argmax((DVg.T>=a).T,axis=2)*dt,np.nan).T)).T
-            hi = np.nanmean(grt[ncond/2:], axis=0)
-            lo = np.nanmean(grt[:ncond/2], axis=0)
-            hilo = [hi[~np.isnan(hi)], lo[~np.isnan(lo)]]
+            rt = (tr+(np.where((DVg.max(axis=2).T>=a).T, np.argmax((DVg.T>=a).T,axis=2)*dt, 999).T)).T
+
             if self.flat:
-                  hilo=[np.hstack(hilo)]
-            # compute RT quantiles for correct and error resp.
-            gq = np.hstack([mq(rtc[rtc<tb], prob=prob)*10 for rtc in hilo])
+                  gq = mq(rt[rt<tb], prob=prob)*10
+            else:
+                  hi = np.hstack(rt[ncond/2:])#, axis=0)
+                  lo = np.hstack(rt[:ncond/2])#, axis=0)
+                  hilo = [hi[hi<tb], lo[lo<tb]]
+                  # compute RT quantiles for correct and error resp.
+                  gq = np.hstack([mq(rti[rti<tb], prob=prob)*10 for rti in hilo])
+
             # Get response and stop accuracy information
-            gac = 1-np.mean(np.where(grt<tb, 1, 0), axis=1)
+            gac = 1-np.mean(np.where(rt<tb, 1, 0), axis=1)
             #return gq, eq, gac, sacc
             return np.hstack([gac, gq])
