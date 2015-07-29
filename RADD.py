fixed opt routine and added flexibility to proactive cost function calculation

RADD.py
@@ -22,39 +22,42 @@ class RADDCore(object):
 
 
 
-      def __init__(self, kind='radd', inits=None, data=None, fit_on='subjects', depends_on=None, niter=50, fit_whole_model=True, *args, **kws):
+      def __init__(self, kind='radd', inits=None, data=None, fit_on='subjects', depends_on=None, niter=50, fit_whole_model=True, tb=None, *args, **kws):
 
             self.data = data
             self.inits = inits
             self.kind = kind
             self.depends_on = depends_on
             self.fit_on = fit_on
+
             if fit_whole_model:
                   self.fit_flat=True
+
             if self.depends_on != None:
                   self.cond=depends_on.values()[0]
                   self.labels=data[self.cond].unique()
                   self.ncond=len(self.labels)
-            else:
-                  self.cond='flat'
-                  self.labels=None
-                  self.ncond=1
 
             if self.kind in ['radd', 'irace']:
-                  ssd = self.data.query('ttype=="stop"').ssd.unique()
+                  ssd = data[data.ttype=="stop"].ssd.unique()
+                  self.pGo = len(data[data.ttype=='go'])/len(data)
                   self.delays = sorted(ssd.astype(np.int))
                   self.ssd = np.array(self.delays)*.001
             elif self.kind=='pro':
                   self.pGo = sorted(self.data.pGo.unique())
                   self.ssd=np.array([.450])
 
+            if tb is None:
+                  self.tb = data[data.response==1].rt.max()
+            else:
+                  self.tb=tb
             if self.fit_on=='bootstrap':
                   self.indx = range(niter)
             else:
-                  self.indx = list(self.data.idx.unique())
+                  self.indx = list(data.idx.unique())
 
 
-      def rangl_data(self, data, re_cut=.650, pro_cut=.54502, kind='radd', prob=np.array([.1, .3, .5, .7, .9])):
+      def rangl_data(self, data, kind='radd', prob=np.array([.1, .3, .5, .7, .9])):
 
             if self.kind in ['radd', 'irace']:
                   gac = data.query('ttype=="go"').acc.mean()
@@ -66,12 +69,19 @@ class RADDCore(object):
                   return np.hstack([gac, sacc, gq*10, eq*10])
 
             elif kind=='pro':
+                  godf = data[data.response==1]
+                  godf['response']=np.where(godf.rt<self.tb, 1, 0)
+                  data = pd.concat([godf, data[data.response==0]])
                   return 1-data.response.mean()
 
 
-      def rt_quantiles(self, data, cutoff=.560, split='HiLo', prob=np.arange(0.1,1.0,0.2)):
+      def rt_quantiles(self, data, split='HiLo', prob=np.arange(0.1,1.0,0.2)):
             rtq = []
-            godf = data[data.response==1]
+
+            godfx = data[data.response==1]
+            godfx['response']=np.where(godfx.rt<self.tb, 1, 0)
+            godf = godfx[godfx.response==1]
+
             if split=='HiLo' and 'HiLo' not in godf.columns:
                   godf['HiLo']=['x']
                   godf[godf['pGo']<=.5, 'HiLo'] = 'Lo'
@@ -79,11 +89,11 @@ class RADDCore(object):
             if split != None:
                   splitdf = godf.groupby(split)
             else:
-                  rts = godf[godf.rt<=cutoff].rt.values
+                  rts = godf[godf.rt<=self.tb].rt.values
                   return mq(rts, prob=prob)*10
 
             for c, df in splitdf:
-                  rts = df[df.rt<=cutoff].rt.values
+                  rts = df[df.rt<=self.tb].rt.values
                   rtq.append(mq(rts, prob=prob)*10)
 
             return np.hstack(rtq)
@@ -112,22 +122,20 @@ class RADDCore(object):
                   return rangl_pro(pd.concat(bootdf_list), rt_cutoff=rt_cutoff)
 
 
-      def set_fitparams(self, ntrials=10000, ftol=1.e-4, xtol=1.e-4, maxfev=1000, niter=500, log_fits=True, disp=True, prob=np.array([.1, .3, .5, .7, .9]), get_params=False, tb=None, **kwgs):
+      def set_fitparams(self, ntrials=10000, ftol=1.e-4, xtol=1.e-4, maxfev=1000, niter=500, log_fits=True, disp=True, prob=np.array([.1, .3, .5, .7, .9]), get_params=False, **kwgs):
 
             if not hasattr(self, 'fitparams'):
                   self.fitparams={}
 
             if self.kind in ['radd', 'irace']:
                   fwts = self.wts.reshape(self.ncond, 16).mean(axis=0)
-                  if tb is None: tb = .650
 
             elif self.kind=='pro':
                   nogo = self.wts[:len(self.pGo)].mean()
                   quant = self.wts[len(self.pGo):].reshape(2, len(prob)).mean(axis=0)
                   fwts = np.hstack([nogo, quant])
-                  if tb is None: tb = .560
 
-            self.fitparams = {'ntrials':ntrials, 'maxfev':maxfev, 'disp':disp, 'ftol':ftol, 'xtol':xtol, 'niter':niter, 'prob':prob, 'log_fits':log_fits, 'tb':tb, 'ssd':self.ssd, 'wts':self.wts, 'ncond':self.ncond, 'flat_wts':fwts}
+            self.fitparams = {'ntrials':ntrials, 'maxfev':maxfev, 'disp':disp, 'ftol':ftol, 'xtol':xtol, 'niter':niter, 'prob':prob, 'log_fits':log_fits, 'tb':self.tb, 'ssd':self.ssd, 'wts':self.wts, 'ncond':self.ncond, 'pGo':self.pGo, 'flat_wts':fwts}
 
             if get_params:
                   return self.fitparams
@@ -237,6 +245,7 @@ class RADDCore(object):
                   qwts_lo = presp[:3].mean()*(np.median(sdlo)/sdlo)
 
                   self.wts = np.hstack([pwts, qwts_hi, qwts_lo])
+                  self.wts[self.wts>4]=self.wts[self.wts<=4].max()
 
 
       def load_default_inits(self):

build.py
@@ -40,18 +40,19 @@ class Model(RADDCore):
                   fit model holding all fixed except depends_on keys
                   or fit model with all free before fitting depends_on keys
 
+            tb (float):
+                  timeboundary: time alloted in task for making a response
+
       """
 
 
-
-
-      def __init__(self, data=pd.DataFrame, kind='radd', inits=None, fit_on='subjects', depends_on=None, niter=50, fit_whole_model=True, weight_presp=True, *args, **kws):
+      def __init__(self, data=pd.DataFrame, kind='radd', inits=None, fit_on='subjects', depends_on=None, niter=50, fit_whole_model=True, weight_presp=True, tb=None, *args, **kws):
 
             self.data=data
             self.weight_presp=weight_presp
             self.is_optimized=False
 
-            super(Model, self).__init__(data=self.data, inits=inits, fit_on=fit_on, depends_on=depends_on, niter=niter, fit_whole_model=fit_whole_model, kind=kind)
+            super(Model, self).__init__(data=self.data, inits=inits, fit_on=fit_on, depends_on=depends_on, niter=niter, fit_whole_model=fit_whole_model, kind=kind, tb=tb)
 
             self.prepare_fit()
 
@@ -60,12 +61,11 @@ class Model(RADDCore):
             """ Method to be used for accessing fitting methods in Optimizer class
             see Optimizer method optimize()
             """
+            fp = self.set_fitparams(xtol=xtol, ftol=xtol, maxfev=maxfev, ntrials=ntrials, niter=niter, disp=disp, log_fits=log_fits, prob=prob, get_params=True)
+            
+            self.opt = Optimizer(dframes=self.dframes, fitparams=fp, kind=self.kind, inits=self.inits, depends_on=self.depends_on, fit_on=self.fit_on, wts=self.wts, pc_map=self.pc_map)
 
-            self.opt = Optimizer(dframes=self.dframes, kind=self.kind, inits=self.inits, depends_on=self.depends_on, fit_on=self.fit_on, wts=self.wts, pc_map=self.pc_map)
-
-            self.fits, self.fitinfo, self.popt = self.opt.optimize(save=save, savepth=savepth, log_fits=log_fits, disp=disp, xtol=xtol, ftol=ftol, maxfev=maxfev, ntrials=ntrials, niter=niter, prob=prob)
-
-            self.is_optimized=True
+            self.fits, self.fitinfo, self.popt = self.opt.optimize_model(save=save, savepth=savepth)
 
 
       def simulate(self):
@@ -76,19 +76,19 @@ class Model(RADDCore):
             except Exception:
                   theta=self.inits
                   self.set_fitparams()
-                  simulator = Simulator(fitparams=self.fitparams, kind=self.kind, inits=theta, pc_map=self.pc_map)
+                  simulator=Simulator(fitparams=self.fitparams, kind=self.kind, inits=theta, pc_map=self.pc_map)
 
             theta = simulator.vectorize_params(theta, sim_info=False, as_dict=True)
 
             if self.kind=='radd':
                   dvg, dvs = simulator.simulate_radd(theta)
-                  yhat = simulator.analyze_reactive(dvg, dvs, theta)
-            if self.kind=='irace':
-                  dvg, dvs = simulator.simulate_irace(theta)
-                  yhat = simulator.analyze_reactive(dvg, dvs, theta)
+                  yhat = simulator.analyze_radd(dvg, dvs, theta)
             elif self.kind=='pro':
                   dvg = simulator.simulate_pro(theta)
-                  yhat = simulator.analyze_proactive(dvg, theta)
+                  yhat = simulator.analyze_pro(dvg, theta)
+            if self.kind=='irace':
+                  dvg, dvs = simulator.simulate_irace(theta)
+                  yhat = simulator.analyze_irace(dvg, dvs, theta)
 
             return yhat
 
@@ -136,14 +136,18 @@ class Optimizer(RADDCore):
       """
 
 
-      def __init__(self, dframes=None, kind='radd', inits=None, fit_on='subjects', depends_on=None, niter=50, fit_whole_model=True, method='nelder', pc_map=None, wts=None, *args, **kws):
+      def __init__(self, dframes=None, kind='radd', inits=None, fit_on='subjects', depends_on=None, niter=50, fit_whole_model=True, method='nelder', pc_map=None, wts=None, fitparams=None, *args, **kws):
 
             self.fits=dframes['fits']
             self.fitinfo=dframes['fitinfo']
-            self.avg_y=dframes['avg_y']
-            self.flat_y=dframes['flat_y']
-            self.dat=dframes['dat']
             self.data=dframes['data']
+            self.fitparams=fitparams
+
+            if fit_on=='average':
+                  self.avg_y=dframes['avg_y']
+                  self.flat_y=dframes['flat_y']
+            elif fit_on in ['subjects', 'bootstrap']:
+                  self.dat=dframes['dat']
 
             self.method=method
             self.wts=wts
@@ -152,18 +156,19 @@ class Optimizer(RADDCore):
             super(Optimizer, self).__init__(kind=kind, data=self.data, fit_on=fit_on, depends_on=depends_on, inits=inits, fit_whole_model=fit_whole_model, niter=niter)
 
 
-      def optimize(self, save=True, savepth='./', log_fits=True, disp=True, xtol=1.e-4, ftol=1.e-4, maxfev=1000, ntrials=10000, niter=500, prob=np.array([.1, .3, .5, .7, .9]), tb=None):
+      def optimize_model(self, save=True, savepth='./'):
 
-            fp = self.set_fitparams(xtol=xtol, ftol=xtol, maxfev=maxfev, ntrials=ntrials, niter=niter, disp=disp, log_fits=log_fits, prob=prob, get_params=True)
+            if self.fitparams is None:
+                  self.set_fitparams()
 
-            self.simulator = Simulator(fitparams=fp, kind=self.kind, inits=self.inits, method=self.method, pc_map=self.pc_map)
+            self.simulator = Simulator(fitparams=self.fitparams, kind=self.kind, inits=self.inits, method=self.method, pc_map=self.pc_map)
 
             if self.fit_on=='average':
-                  self.yhat, self.fitinfo, self.popt = self.__opt_routine__(self.avg_y)
-                  return self.yhat, self.fitinfo, self.popt
+                  yhat, fitinfo, popt = self.__opt_routine__(self.avg_y)
+                  return yhat, fitinfo, popt
             else:
-                  self.__indx_optimize__(save=save, savepth=savepth)
-                  return self.fits, self.fitinfo, self.popt
+                  fits, fitinfo, popt = self.__indx_optimize__(save=save, savepth=savepth, fitparams=fp)
+                  return fits, fitinfo, popt
 
 
       def optimize_theta(self, y, inits, flat=False):
@@ -227,6 +232,7 @@ class Optimizer(RADDCore):
             return  yhat, finfo, popt
 
 
+
       def set_bounds(self, a=(.001, 1.000), tr=(.001, .550), v=(.0001, 4.0000), z=(.001, .900), ssv=(-4.000, -.0001)):
 
             """
@@ -243,16 +249,26 @@ class Optimizer(RADDCore):
             return bounds
 
 
+
       def __indx_optimize__(self, save=True, savepth='./'):
 
-            ri=0; radv=self.ncond
+            ri=0; nc=self.ncond
             pcols=self.fitinfo.columns
             for i, y in enumerate(self.dat):
+
+                  if self.kind in ['radd', 'irace']:
+                        self.flat_y = y.mean(axis=0)
+                  elif self.kind=='pro':
+                        nquant = len(self.fitparams['prob'])
+                        flatgo = y[:nc].mean(),
+                        flatq = y[nc:].reshape(2,nquant).mean(axis=0)
+                        self.flat_y = np.hstack([flatgo, flatq])
+
                   yhat, finfo, popt = self.__opt_routine__(y)
                   self.fitinfo.iloc[i]=pd.Series({pc: finfo[pc] for pc in pcols})
                   if self.kind in ['radd', 'irace']:
-                        self.fits.iloc[ri:ri+radv, radv:] = yhat
-                        ri+=radv
+                        self.fits.iloc[ri:ri+nc, nc:] = yhat
+                        ri+=nc
                   else:
                         self.fits.iloc[i] = yhat
                   if save:
@@ -262,25 +278,24 @@ class Optimizer(RADDCore):
             self.popt=self.fitinfo.mean()
 
 
+
       def __opt_routine__(self, y):
 
             p = dict(deepcopy(self.inits))
             fp = self.fitparams
-
             if not self.fit_flat:
                   self.simulator.ncond = self.ncond
                   self.simulator.wts = self.wts
                   yhat, finfo, popt = self.optimize_theta(y=y, inits=p, flat=False)
             else:
-
                   to_fit = [self.flat_y, y]
                   wts = [fp['flat_wts'], fp['wts']]
                   flat=[1, 0]
                   ncond=[1, self.ncond]
-                  for i, y in enumerate(to_fit):
+                  for i, yi in enumerate(to_fit):
                         self.simulator.ncond = ncond[i]
                         self.simulator.wts = wts[i]
-                        yhat, finfo, popt = self.optimize_theta(y=y, inits=p, flat=flat[i])
+                        yhat, finfo, popt = self.optimize_theta(y=yi, inits=p, flat=flat[i])
                         p = deepcopy(popt)
 
             return yhat, finfo, popt

fit.py
@@ -101,6 +101,7 @@ class Simulator(object):
                         out.extend([Ps, Ts])
             return out
 
+
       def set_bounds(self, a=(.001, 1.000), tr=(.001, .550), v=(.0001, 4.0000), z=(.001, .900), ssv=(-4.000, -.0001)):
 
             """
@@ -127,6 +128,21 @@ class Simulator(object):
                   self.costfx = self.pro_costfx
 
 
+      def radd_costfx(self, theta):
+            """
+            Reactive Model (RADD) cost function
+            """
+
+            if type(theta)==dict:
+                  p = {k:theta[k] for k in theta.keys()}
+            else:
+                  p = theta.valuesdict()
+            dvg, dvs = self.simulate_radd(p)
+            yhat = self.analyze_radd(dvg, dvs, p)
+
+            return (self.y - yhat)*self.wts[:len(self.y)]
+
+
       def pro_costfx(self, theta):
             """
             Proactive model cost function
@@ -136,7 +152,6 @@ class Simulator(object):
                   p = {k:theta[k] for k in theta.keys()}
             else:
                   p = theta.valuesdict()
-
             dvg = self.simulate_pro(p)
             yhat = self.analyze_pro(dvg, p)
 
@@ -151,24 +166,8 @@ class Simulator(object):
                   p = {k:theta[k] for k in theta.keys()}
             else:
                   p = theta.valuesdict()
-
             dvg, dvs = self.simulate_irace(p)
-            yhat = self.analyze_reactive(dvg, dvs, p)
-            return (self.y - yhat)*self.wts[:len(self.y)]
-
-
-      def radd_costfx(self, theta):
-            """
-            Reactive Model (RADD) cost function
-            """
-
-            if type(theta)==dict:
-                  p = {k:theta[k] for k in theta.keys()}
-            else:
-                  p = theta.valuesdict()
-
-            dvg, dvs = self.simulate_radd(p)
-            yhat = self.analyze_reactive(dvg, dvs, p)
+            yhat = self.analyze_irace(dvg, dvs, p)
 
             return (self.y - yhat)*self.wts[:len(self.y)]
 
@@ -180,7 +179,6 @@ class Simulator(object):
             DVg = z + np.cumsum(np.where((rs((self.ncond, self.ntot, Tg.max())).T < Pg), self.dx, -self.dx).T, axis=2)
             init_ss = np.array([[DVc[:self.nss, ix] for ix in np.where(Ts<Tg[i], Tg[i]-Ts, 0)] for i, DVc in enumerate(DVg)])
             DVs = init_ss[:, :, :, None] + np.cumsum(np.where(rs((self.nss, Ts.max()))<Ps, self.dx, -self.dx), axis=1)
-
             return DVg, DVs
 
 
@@ -200,22 +198,17 @@ class Simulator(object):
             DVg = z + np.cumsum(np.where((rs((self.ncond, self.ntot, Tg.max())).T < Pg), self.dx, -self.dx).T, axis=2)
             init_ss = np.ones((self.ncond, self.nssd, self.nss))*z
             DVs = init_ss[:, :, :, None] + np.cumsum(np.where(rs((self.nss, Ts.max()))<Ps, self.dx, -self.dx), axis=1)
-
             return DVg, DVs
 
 
-      def analyze_reactive(self, DVg, DVs, p):
+      def analyze_radd(self, DVg, DVs, p):
 
             a, tr, v, ssv, z = self.vectorize_params(p, sim_info=False)
             dt=self.dt; nss=self.nss; ncond=self.ncond; ssd=self.ssd; tb=self.tb; prob=self.prob
 
             grt = (tr+(np.where((DVg[:, nss:, :].max(axis=2).T>=a).T, np.argmax((DVg[:, nss:, :].T>=a).T,axis=2)*dt, np.nan).T)).T
             ertx = (tr+(np.where((DVg[:, :nss, :].max(axis=2).T>=a).T, np.argmax((DVg[:, :nss, :].T>=a).T,axis=2)*dt, np.nan).T)).T
-
-            if self.kind=='radd':
-                  ssrt = np.where(np.any(DVs<=0, axis=3), ssd[:, None]+np.argmax(DVs<=0, axis=3)*dt,np.nan)
-            else:
-                  ssrt = ((np.where((DVs.max(axis=3).T>=a).T,ssd[:, None]+np.argmax((DVs.T>=a).T,axis=3)*dt,np.nan).T)).T
+            ssrt = np.where(np.any(DVs<=0, axis=3), ssd[:, None]+np.argmax(DVs<=0, axis=3)*dt,np.nan)
 
             # compute RT quantiles for correct and error resp.
             ert = np.array([ertx[i] * np.ones_like(ssrt[i]) for i in range(ncond)])
@@ -224,18 +217,17 @@ class Simulator(object):
             # Get response and stop accuracy information
             gac = np.nanmean(np.where(grt<tb, 1, 0), axis=1)
             sacc = np.where(ert<ssrt, 0, 1).mean(axis=2)
-
             return np.hstack([np.hstack([i[ii] for i in [gac, sacc, gq, eq]]) for ii in range(ncond)])
 
 
-      def analyze_proactive(self, DVg, p):
+      def analyze_pro(self, DVg, p):
 
             a, tr, v, z = self.vectorize_params(p, sim_info=False)
             dt=self.dt; ncond=self.ncond; tb=self.tb; prob=self.prob
 
             rt = (tr+(np.where((DVg.max(axis=2).T>=a).T, np.argmax((DVg.T>=a).T,axis=2)*dt, 999).T)).T
 
-            if self.flat:
+            if len(self.y)<=(len(prob)+1):
                   gq = mq(rt[rt<tb], prob=prob)*10
             else:
                   hi = np.hstack(rt[ncond/2:])#, axis=0)
@@ -246,5 +238,23 @@ class Simulator(object):
 
             # Get response and stop accuracy information
             gac = 1-np.mean(np.where(rt<tb, 1, 0), axis=1)
-            #return gq, eq, gac, sacc
             return np.hstack([gac, gq])
+
+
+      def analyze_irace(self, DVg, DVs, p):
+
+            a, tr, v, ssv, z = self.vectorize_params(p, sim_info=False)
+            dt=self.dt; nss=self.nss; ncond=self.ncond; ssd=self.ssd; tb=self.tb; prob=self.prob
+
+            grt = (tr+(np.where((DVg[:, nss:, :].max(axis=2).T>=a).T, np.argmax((DVg[:, nss:, :].T>=a).T,axis=2)*dt, np.nan).T)).T
+            ertx = (tr+(np.where((DVg[:, :nss, :].max(axis=2).T>=a).T, np.argmax((DVg[:, :nss, :].T>=a).T,axis=2)*dt, np.nan).T)).T
+            ssrt = ((np.where((DVs.max(axis=3).T>=a).T,ssd[:, None]+np.argmax((DVs.T>=a).T,axis=3)*dt,np.nan).T)).T
+
+            # compute RT quantiles for correct and error resp.
+            ert = np.array([ertx[i] * np.ones_like(ssrt[i]) for i in range(ncond)])
+            gq = np.vstack([mq(rtc[rtc<tb], prob=prob)*10 for rtc in grt])
+            eq = [mq(ert[i][ert[i]<ssrt[i]], prob=prob)*10 for i in range(ncond)]
+            # Get response and stop accuracy information
+            gac = np.nanmean(np.where(grt<tb, 1, 0), axis=1)
+            sacc = np.where(ert<ssrt, 0, 1).mean(axis=2)
+            return np.hstack([np.hstack([i[ii] for i in [gac, sacc, gq, eq]]) for ii in range(ncond)])

vis.py
@@ -19,10 +19,10 @@ gpal = lambda nc: sns.blend_palette(['#65b88f', '#16a085'], n_colors=nc)
 ppal = lambda nc: sns.blend_palette(['#848bb6', '#674172'], n_colors=nc)
 
 
-def scurves(lines=[], kind='react', yerr=[], pstop=.5, ax=None, linestyles=None, colors=None, markers=False, labels=None):
+def scurves(lines=[], kind='pro', yerr=[], pstop=.5, ax=None, linestyles=None, colors=None, markers=False, labels=None):
 
       if len(lines[0])==6:
-                task='proact'
+                kind=='pro'
 
       if ax is None:
             f, ax = plt.subplots(1, figsize=(6,5))
@@ -36,7 +36,7 @@ def scurves(lines=[], kind='react', yerr=[], pstop=.5, ax=None, linestyles=None,
       lines=[np.array(line) if type(line)==list else line for line in lines]
       pse=[];
 
-      if kind=='react':
+      if kind=='radd':
             x=np.array([400, 350, 300, 250, 200], dtype='float')
             xtls=x.copy()[::-1]; xsim=np.linspace(15, 50, 10000);
             yylabel='P(Stop)'; scale_factor=100; xxlabel='SSD'; xxlim=(18,42)
@@ -81,7 +81,7 @@ def scurves(lines=[], kind='react', yerr=[], pstop=.5, ax=None, linestyles=None,
 
 
 
-def plot_fits(y, yhat, bw=.1, plot_acc=False, save=True, kind='react', savestr='fit_plot_rtq'):
+def plot_fits(y, yhat, bw=.1, plot_acc=False, save=True, kind='radd', savestr='fit_plot'):
 
       sns.set_context('notebook', font_scale=1.6)
 
@@ -92,8 +92,7 @@ def plot_fits(y, yhat, bw=.1, plot_acc=False, save=True, kind='react', savestr='
 
       if plot_acc:
             f, (ax1, ax2) = plt.subplots(1,2,figsize=(10, 5.5))
-            savestr = savestr + "_acc"
-            if kind=='react':
+            if kind=='radd':
                   gacc = y[0]
                   sacc = y[1:6]
                   fit_gacc = yhat[0]
@@ -109,7 +108,7 @@ def plot_fits(y, yhat, bw=.1, plot_acc=False, save=True, kind='react', savestr='
       quant_list = [gq, fit_gq, eq, fit_eq]
       kdefits = [utils.kde_fit_quantiles(q, bw=bw) for q in quant_list]
 
-      if kind=='react':
+      if kind=='radd':
             lbs=['Data Cor','Fit Cor','Data Err','Fit Err']
       else:
             lbs=['Data Hi', 'Fit Hi', 'Data Lo', 'Fit Lo']