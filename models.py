b52f2184b6141ab8cbb8cbaf40c746afab8e944f
dunovank <dunovank@gmail.com>
2015-08-01 00:19:31 -0400

    moved Optimizer class to optimize and renamed fit.py as models.py

---
+++ models.py
@@ -0,0 +1,291 @@
+#!/usr/local/bin/env python
+from __future__ import division
+import time
+from copy import deepcopy
+import numpy as np
+from numpy.random import random_sample as rs
+from scipy.stats.mstats import mquantiles as mq
+
+
+class Simulator(object):
+
+      """
+      Core code for simulating RADD models
+
+            * All cond, SSD, & timepoints are simulated simultaneously
+
+            * a, tr, and v parameters are initialized as vectors,
+            1 x Ncond so Optimizer can optimize a single costfx
+            for multiple conditions.
+      """
+
+      def __init__(self, model=None, fitparams=None, inits=None, pc_map=None, kind='radd', prepare=True, is_flat=False):
+
+            self.dt=.0005
+            self.si=.01
+            self.dx=np.sqrt(self.si*self.dt)
+            self.is_flat=is_flat
+
+            if model != None:
+                  self.fitparams = model.fitparams
+                  self.inits = model.inits
+                  self.kind=model.kind
+                  self.pc_map = model.pc_map
+            else:
+                  self.fitparams=fitparams
+                  self.inits=inits
+                  self.kind=kind
+                  self.pc_map=pc_map
+
+            pdepends = self.fitparams['depends_on'].keys()
+            self.pnames=['a', 'tr', 'v', 'ssv', 'z', 'xb']
+            self.pvc=deepcopy(['a', 'tr', 'v', 'xb'])
+            #for pdep in pdepends:
+            #      self.pvc.remove(pdep)
+
+
+            if prepare:
+                  self.prepare_simulator()
+
+
+      def prepare_simulator(self):
+
+            fp=dict(deepcopy(self.fitparams))
+            self.tb=fp['tb']
+            self.wts=fp['wts']
+            self.ncond=fp['ncond']
+            self.ntot=fp['ntrials']
+            self.prob=fp['prob']
+            self.ssd=fp['ssd']
+            self.scale=fp['scale']
+            self.nssd=len(self.ssd)
+            self.nss=int(.5*self.ntot)
+            self.lowerb=0
+            self.y=None
+
+            #self.pfit = list(set(self.inits.keys()).intersection(self.pnames))
+            if 'pro' in self.kind:
+                  self.ntot = int(self.ntot/self.ncond)
+
+
+      def vectorize_params(self, p, as_dict=False):
+
+            """
+            ensures that all parameters are either converted to arrays
+            of length ncond.
+
+            * can also be accessed directly, outside of optimization routine,
+            to generate vectorized parameter dictionaries for simulating.
+
+            * Parameters (p[pkey]=pval) that are constant across conditions
+            are broadcast as [pval]*n. Conditional parameters are treated as arrays with
+            distinct values [pval_1...pval_n], one for each condition.
+
+            * caculates drift coefficients (Pg & Ps)
+            * calculates number of timepoints from tr/ssd to tb
+
+            pc_map (dict):          keys: conditional parameter names (i.e. 'v')
+                                    values: keys + condition names ('v_bsl, v_pnl')
+
+            pvc (list):             list of non conditional parameter names
+
+            as_dict (bool):         return vect. param dictionaries for simulating
+
+            sim_info (bool):        return drift coeff, ntimepoints
+
+            """
+
+            #if self.is_flat:
+            #      return p
+            if 'xb' not in p.keys():
+                  p['xb']=1.0
+
+            for pkey in self.pvc:
+                  p[pkey]=np.ones(self.ncond)*p[pkey]
+
+            for pkey, pkc in self.pc_map.items():
+                  if self.ncond==1:
+                        break
+                  elif pkc[0] not in p.keys():
+                        p[pkey] = p[pkey]*np.ones(len(pkc))
+                  else:
+                        p[pkey] = np.array([p[pc] for pc in pkc])
+
+            if 'z' in p.keys():
+                  self.lowerb = p['z']
+
+            return p
+
+
+      def __update_go_process__(self, p):
+
+            Pg = 0.5*(1 + p['v']*self.dx/self.si)
+            Tg = np.ceil((self.tb-p['tr'])/self.dt).astype(int)
+            t = np.cumsum([self.dt]*Tg.max())
+            if 'x' in self.kind:
+                  self.xtb = np.array([np.exp(xtb*t) for xtb in p['xb']])
+            else:
+                  self.xtb = np.array([np.ones(len(t)) for i in range(self.ncond)])
+
+            return Pg, Tg
+
+      def __update_stop_process__(self, p):
+
+            Ps = 0.5*(1 + p['ssv']*self.dx/self.si)
+            Ts = np.ceil((self.tb-self.ssd)/self.dt).astype(int)
+            return Ps, Ts
+
+
+      def set_costfx(self):
+
+            if 'radd' in self.kind:
+                  self.costfx = self.radd_costfx
+            elif 'pro' in self.kind:
+                  self.costfx = self.pro_costfx
+            elif 'irace' in self.kind:
+                  self.costfx = self.irace_costfx
+
+
+      def radd_costfx(self, theta):
+            """
+            Reactive Model (RADD) cost function
+            """
+
+            if type(theta)==dict:
+                  p = dict(deepcopy(theta))
+            else:
+                  p = theta.valuesdict()
+
+            p = self.vectorize_params(p)
+            dvg, dvs = self.simulate_radd(p)
+            yhat = self.analyze_radd(dvg, dvs, p)
+
+            return (yhat - self.y)*self.wts[:len(self.y)]
+
+
+      def pro_costfx(self, theta):
+            """
+            Proactive model cost function
+            """
+
+            if type(theta)==dict:
+                  p = dict(deepcopy(theta))
+            else:
+                  p = theta.valuesdict()
+
+            p = self.vectorize_params(p)
+            dvg = self.simulate_pro(p)
+            yhat = self.analyze_pro(dvg, p)
+
+            return (yhat - self.y)*self.wts[:len(self.y)]
+
+
+      def irace_costfx(self, theta):
+            """
+            Independent Race Model cost function
+            """
+            if type(theta)==dict:
+                  p = dict(deepcopy(theta))
+            else:
+                  p = theta.valuesdict()
+
+            p = self.vectorize_params(p)
+            dvg, dvs = self.simulate_irace(p)
+            yhat = self.analyze_irace(dvg, dvs, p)
+
+            return (yhat - self.y)*self.wts[:len(self.y)]
+
+
+      def simulate_radd(self, p):
+
+            Pg, Tg = self.__update_go_process__(p)
+            Ps, Ts = self.__update_stop_process__(p)
+
+            # a/tr/v Bias: ALL CONDITIONS, ALL SSD
+            DVg = self.lowerb + (self.xtb[:,None] * np.cumsum(np.where((rs((self.ncond, self.ntot, Tg.max())).T < Pg), self.dx, - self.dx).T, axis=2))
+            init_ss = np.array([[DVc[:self.nss, ix] for ix in np.where(Ts<Tg[i], Tg[i]-Ts, 0)] for i, DVc in enumerate(DVg)])
+            DVs = init_ss[:, :, :, None] + np.cumsum(np.where(rs((self.nss, Ts.max()))<Ps, self.dx, -self.dx), axis=1)
+            return DVg, DVs
+
+
+      def simulate_pro(self, p):
+
+            Pg, Tg = self.__update_go_process__(p)
+
+            # a/tr/v Bias: ALL CONDITIONS
+            DVg = self.xtb[:,None]*np.cumsum(np.where((rs((self.ncond, self.ntot, Tg.max())).T < Pg), self.dx, -self.dx).T, axis=2)
+            return DVg
+
+
+      def simulate_irace(self, p):
+
+            Pg, Tg = self.__update_go_process__(p)
+            Ps, Ts = self.__update_stop_process__(p)
+
+            # a/tr/v Bias: ALL CONDITIONS, ALL SSD
+            DVg = self.lowerb + (self.xtb[:,None] * np.cumsum(np.where((rs((self.ncond, self.ntot, Tg.max())).T < Pg), self.dx, -self.dx).T, axis=2))
+            init_ss = self.lowerb*np.ones((self.ncond, self.nssd, self.nss))
+            DVs = init_ss[:, :, :, None] + np.cumsum(np.where(rs((self.nss, Ts.max())) < Ps, self.dx, -self.dx), axis=1)
+
+            return DVg, DVs
+
+
+      def analyze_radd(self, DVg, DVs, p):
+
+            dt=self.dt; nss=self.nss; ncond=self.ncond; ssd=self.ssd;
+            tb=self.tb; prob=self.prob; scale=self.scale; a=p['a']; tr=p['tr']
+
+            grt = (tr+(np.where((DVg[:, nss:, :].max(axis=2).T>=a).T, np.argmax((DVg[:, nss:, :].T>=a).T,axis=2)*dt, np.nan).T)).T
+            ertx = (tr+(np.where((DVg[:, :nss, :].max(axis=2).T>=a).T, np.argmax((DVg[:, :nss, :].T>=a).T,axis=2)*dt, np.nan).T)).T
+            ssrt = np.where(np.any(DVs<=0, axis=3), ssd[:, None]+np.argmax(DVs<=0, axis=3)*dt,np.nan)
+
+            # compute RT quantiles for correct and error resp.
+            ert = np.array([ertx[i] * np.ones_like(ssrt[i]) for i in range(ncond)])
+            gq = np.vstack([mq(rtc[rtc<tb], prob=prob)*scale for rtc in grt])
+            eq = [mq(ert[i][ert[i]<ssrt[i]], prob=prob)*scale for i in range(ncond)]
+            # Get response and stop accuracy information
+            gac = np.nanmean(np.where(grt<tb, 1, 0), axis=1)
+            sacc = np.where(ert<ssrt, 0, 1).mean(axis=2)
+
+            return np.hstack([np.hstack([i[ii] for i in [gac, sacc, gq, eq]]) for ii in range(ncond)])
+
+
+      def analyze_pro(self, DVg, p):
+
+            dt=self.dt; ncond=self.ncond; tb=self.tb; prob=self.prob;
+            scale=self.scale; a=p['a']; tr=p['tr']
+
+            rt = (tr+(np.where((DVg.max(axis=2).T>=a).T, np.argmax((DVg.T>=a).T,axis=2)*dt, 999).T)).T
+
+            if self.is_flat:
+                  qrt = mq(rt[rt<tb], prob=prob)*scale
+            else:
+                  hi = np.hstack(rt[3:])
+                  lo = np.hstack(rt[1:3])
+                  hilo = [hi[hi<tb], lo[lo<tb]]
+                  # compute RT quantiles for correct and error resp.
+                  qrt = np.hstack([mq(rti[rti<tb], prob=prob)*scale for rti in hilo])
+
+            # Get response and stop accuracy information
+            gac = 1-np.mean(np.where(rt<tb, 1, 0), axis=1)
+            return np.hstack([gac, qrt])
+
+
+
+      def analyze_irace(self, DVg, DVs, p):
+
+            dt=self.dt; nss=self.nss; ncond=self.ncond; ssd=self.ssd;
+            tb=self.tb; prob=self.prob; scale=self.scale; a=p['a']; tr=p['tr']
+
+            grt = (tr+(np.where((DVg[:, nss:, :].max(axis=2).T>=a).T, np.argmax((DVg[:, nss:, :].T>=a).T,axis=2)*dt, np.nan).T)).T
+            ertx = (tr+(np.where((DVg[:, :nss, :].max(axis=2).T>=a).T, np.argmax((DVg[:, :nss, :].T>=a).T,axis=2)*dt, np.nan).T)).T
+            ssrt = ((np.where((DVs.max(axis=3).T>=a).T,ssd[:, None]+np.argmax((DVs.T>=a).T,axis=3)*dt,np.nan).T)).T
+
+            # compute RT quantiles for correct and error resp.
+            ert = np.array([ertx[i] * np.ones_like(ssrt[i]) for i in range(ncond)])
+            gq = np.vstack([mq(rtc[rtc<tb], prob=prob)*scale for rtc in grt])
+            eq = [mq(ert[i][ert[i]<ssrt[i]], prob=prob)*scale for i in range(ncond)]
+            # Get response and stop accuracy information
+            gac = np.nanmean(np.where(grt<tb, 1, 0), axis=1)
+            sacc = np.where(ert<ssrt, 0, 1).mean(axis=2)
+            return np.hstack([np.hstack([i[ii] for i in [gac, sacc, gq, eq]]) for ii in range(ncond)])
---
+++ optimize.py
@@ -0,0 +1,197 @@
+#!/usr/local/bin/env python
+from __future__ import division
+import os
+import time
+from copy import deepcopy
+import numpy as np
+import pandas as pd
+from radd.models import Simulator
+from radd.RADD import RADDCore
+from lmfit import Parameters, minimize, fit_report, Minimizer
+
+
+class Optimizer(RADDCore):
+
+      """ Optimizer class acts as interface between Model and Simulator (see fit.py) objects.
+      Structures fitting routines so that Models are first optimized with the full set of
+      parameters free, data collapsing across conditions.
+
+      The fitted parameters are then used as the initial parameters for fitting conditional
+      models with only a subset of parameters are left free to vary across levels of a given
+      experimental condition.
+
+      Parameter dependencies are specified when initializing Model object via
+      <depends_on> arg (i.e.{parameter: condition})
+
+      Handles fitting routines for models of average, individual subject, and bootstrapped data
+      """
+
+      def __init__(self, dframes=None, fitparams=None, kind='radd', inits=None, fit_on='subjects', depends_on=None, niter=50, fit_whole_model=True, method='nelder', pc_map=None, wts=None, *args, **kws):
+
+            self.fits=dframes['fits']
+            self.fitinfo=dframes['fitinfo']
+            self.data=dframes['data']
+            self.fitparams=fitparams
+
+            if fit_on=='average':
+                  self.avg_y=dframes['avg_y']
+                  self.flat_y=dframes['flat_y']
+            elif fit_on in ['subjects', 'bootstrap']:
+                  self.dat=dframes['dat']
+
+            self.method=method
+            self.wts=wts
+            self.pc_map=pc_map
+
+            super(Optimizer, self).__init__(kind=kind, data=self.data, fit_on=fit_on, depends_on=depends_on, inits=inits, fit_whole_model=fit_whole_model, niter=niter)
+
+
+      def optimize_model(self, save=True, savepth='./'):
+
+            if self.fitparams is None:
+                  self.set_fitparams()
+
+            self.simulator = Simulator(fitparams=self.fitparams, kind=self.kind, inits=self.inits, pc_map=self.pc_map)
+
+            if self.fit_on=='average':
+                  yhat, fitinfo, popt = self.__opt_routine__(self.avg_y)
+                  return yhat, fitinfo, popt
+            else:
+                  fits, fitinfo, popt = self.__indx_optimize__(save=save, savepth=savepth, fitparams=fp)
+                  return fits, fitinfo, popt
+
+
+      def optimize_theta(self, y, inits, flat=False):
+
+            """
+            Optimizes parameters following specified parameter
+            dependencies on task conditions (i.e. depends_on={param: cond})
+            """
+
+            self.simulator.y = y.flatten()
+            self.simulator.set_costfx()
+            self.simulator.is_flat = flat
+
+            pnames = deepcopy(self.simulator.pnames)
+            pfit = list(set(inits.keys()).intersection(pnames))
+
+            lim = self.set_bounds()
+            fp = self.fitparams
+
+            ip = deepcopy(inits)
+            if self.kind=='irace':
+                  ip['ssv']=abs(ip['ssv'])
+            elif self.kind=='radd':
+                  ip['ssv']=-abs(ip['ssv'])
+
+            theta=Parameters()
+            for pkey, pc_list in self.pc_map.items():
+                  if flat: break
+                  self.simulator.pvc.remove(pkey)
+                  pfit.remove(pkey)
+                  mn = lim[pkey][0]; mx=lim[pkey][1]
+                  d0 = [theta.add(pc, value=ip[pkey], vary=1, min=mn, max=mx) for pc in pc_list]
+
+            p0 = [theta.add(k, value=ip[k], vary=flat, min=lim[k][0], max=lim[k][1]) for k in pfit]
+            opt_kws = {'disp':fp['disp'], 'xtol':fp['xtol'], 'ftol':['ftol'], 'maxfev':fp['maxfev']}
+
+            # OPTIMIZE THETA
+            optmod = minimize(self.simulator.costfx, theta, method=self.method, options=opt_kws)
+
+            optp = optmod.params
+            finfo = {k:optp[k].value for k in optp.keys()}
+            popt = deepcopy(finfo)
+
+            finfo['chi'] = optmod.chisqr
+            finfo['rchi'] = optmod.redchi
+            finfo['CNVRG'] = optmod.pop('success')
+            finfo['nfev'] = optmod.pop('nfev')
+            finfo['AIC']=optmod.aic
+            finfo['BIC']=optmod.bic
+            self.residual = optmod.residual
+            yhat = y.flatten() + self.residual
+
+            pkeys = self.depends_on.keys()
+            fitid=self.kind + ": %s" % str(tuple(pkeys))
+
+            if fp['log_fits']:
+                  with open('fit_report.txt', 'a') as f:
+                        f.write(str(fitid)+'\n')
+                        f.write(fit_report(optmod, show_correl=False)+'\n')
+                        f.write('AIC: %.8f' % optmod.aic + '\n')
+                        f.write('BIC: %.8f' % optmod.bic + '\n')
+                        f.write('chi: %.8f' % optmod.chisqr + '\n')
+                        f.write('rchi: %.8f' % optmod.redchi + '\n')
+                        f.write('Converged: %s' % finfo['CNVRG'] + '\n')
+                        f.write('--'*20+'\n\n')
+
+            return  yhat, finfo, popt
+
+
+
+      def set_bounds(self, a=(.001, 1.000), tr=(.001, .550), v=(.0001, 4.0000), z=(.001, .900), ssv=(-4.000, -.0001), xb=(.01,10)):
+
+            """
+            set and return boundaries to limit search space
+            of parameter optimization in <optimize_theta>
+            """
+            if self.kind=='irace':
+                  ssv=(abs(ssv[1]), abs(ssv[0]))
+
+            bounds = {'a': a, 'tr': tr, 'v': v, 'ssv': ssv, 'z': z, 'xb':xb}
+            return bounds
+
+
+
+      def __indx_optimize__(self, save=True, savepth='./'):
+
+            ri=0; nc=self.ncond
+            pcols=self.fitinfo.columns
+            for i, y in enumerate(self.dat):
+
+                  if self.kind in ['radd', 'irace']:
+                        self.flat_y = y.mean(axis=0)
+                  elif self.kind in ['pro', 'xpro']:
+                        nquant = len(self.fitparams['prob'])
+                        flatgo = y[:nc].mean(),
+                        flatq = y[nc:].reshape(2,nquant).mean(axis=0)
+                        self.flat_y = np.hstack([flatgo, flatq])
+
+                  # OPTIMIZE IDX MODEL
+                  yhat, finfo, popt = self.__opt_routine__(y)
+
+                  self.fitinfo.iloc[i]=pd.Series({pc: finfo[pc] for pc in pcols})
+                  if self.kind in ['radd', 'irace']:
+                        self.fits.iloc[ri:ri+nc, nc:] = yhat
+                        ri+=nc
+                  elif 'pro' in self.kind:
+                        self.fits.iloc[i] = yhat
+                  if save:
+                        self.fits.to_csv(savepth+"fits.csv")
+                        self.fitinfo.to_csv(savepth+"fitinfo.csv")
+
+            self.popt=self.fitinfo.mean()
+
+
+
+      def __opt_routine__(self, y):
+
+            p = dict(deepcopy(self.inits))
+            fp = self.fitparams
+
+            if not self.fit_flat:
+                  self.simulator.ncond = self.ncond
+                  self.simulator.wts = self.wts
+                  yhat, finfo, popt = self.optimize_theta(y=y, inits=p, flat=False)
+            else:
+                  to_fit = [self.flat_y, y]
+                  wts = [fp['flat_wts'], fp['wts']]
+                  flat=[1, 0]
+                  ncond=[1, self.ncond]
+                  for i, yi in enumerate(to_fit):
+                        self.simulator.ncond = ncond[i]
+                        self.simulator.wts = wts[i]
+                        yhat, finfo, popt = self.optimize_theta(y=yi, inits=p, flat=flat[i])
+                        p = deepcopy(popt)
+
+            return yhat, finfo, popt
