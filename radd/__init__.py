137eeff031125d7ac362bbf1db6f7a07bf87241d
Kyle Dunovan <dunovank@gmail.com>
2016-06-10 03:16:31 -0400

    updated radd version number in __init__.py

--- radd/__init__.py
+++ radd/__init__.py
@@ -4,4 +4,4 @@ import glob
 modules = glob.glob(os.path.dirname(__file__) + "/*.py")
 __all__ = [os.path.basename(f)[:-3] for f in modules]
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import numpy as np
 
 setup(
     name='RADD',
-    version='0.0.4',
+    version='0.0.5',
     author='Kyle Dunovan, Timothy Verstynen',
     author_email='dunovank@gmail.com',
     url='http://github.com/CoAxLab/radd',
