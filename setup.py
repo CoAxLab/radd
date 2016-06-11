eab53a9b47139d50e64921205d265f309475fd31
dunovank <dunovank@gmail.com>
2016-06-10 22:31:28 -0400

    hopefully imports are fixed now, v0.0.6

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import numpy as np
 
 setup(
@@ -7,9 +7,8 @@ setup(
     author='Kyle Dunovan, Timothy Verstynen',
     author_email='dunovank@gmail.com',
     url='http://github.com/CoAxLab/radd',
-    packages=['radd', 'radd.tools', 'radd.rl'],
+    packages = find_packages(),
     description='RADD (Race Against Drift-Diffusion) is a python package for fitting & simulating cognitive models of reinforcement learning and decision-making',
     install_requires=['NumPy >=1.8.2', 'SciPy >= 0.16.1', 'matplotlib >= 1.4.3', 'seaborn>=0.5.1', 'pandas >= 0.12.0', 'lmfit>=0.9.0'],
-    include_package_data=True,
     include_dirs = [np.get_include()],
 )
